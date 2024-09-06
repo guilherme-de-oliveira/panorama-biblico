1a4bcdaf97ee4895ae59fe327c530ca97116d862
Guilherme Henrique de Oliveira <guihenrique_oliveira@hotmail.com>
2024-09-05 22:40:33 -0300

    Revert "feat(multiple): icons and model"
    This reverts commit 51fae345fb91472a700e0a3faf3b5833f88c08cf.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 export interface Slide {
-  title: string;
-  subject?: string;
   id: number;
+  title: string;
+  icon?: string; // Only for menu items
+  img?: string;
+  next?: number;
+  prev?: number;
 }
 
 @Injectable({
@@ -12,19 +15,69 @@ export interface Slide {
 export class DataService {
   public slides: Slide[] = [
     {
-      title: 'Jesus, o filho de Deus',
-      // subject: 'New event: Trip to Vegas',
-      id: 1
+      id: 1,
+      title: 'Jesus, o Filho de Deus',
+      icon: 'Leao',
+      img: '03 - Leao',
+      next: 2,
+      prev: 0
     },
     {
+      id: 2,
+      title: 'Jesus, o Filho de Deus',
+      img: '04 - 1 Cordeiro',
+      next: 3,
+      prev: 1
+    },
+    {
+      id: 3,
+      title: 'Jesus, o Filho de Deus',
+      img: '05 - 2 Cordeiro',
+      next: 4,
+      prev: 2
+    },
+    {
+      id: 4,
+      title: 'Jesus, o Filho de Deus',
+      img: '06 - 3 Cordeiro',
+      next: 5,
+      prev: 3
+    },
+    {
+      id: 5,
+      title: 'Jesus, o Filho de Deus',
+      img: '07 - 4 Cordeiro',
+      next: 6,
+      prev: 4
+    },
+    {
+      id: 6,
+      title: 'Jesus, o Filho de Deus',
+      img: '08 - O Cavaleiro de Cavalo Branco',
+      next: 7,
+      prev: 5
+    },
+    {
+      id: 7,
       title: 'Princípio',
-      // subject: 'New event: Trip to Vegas',
-      id: 2
+      icon: 'Principio',
+      img: '1 - Principio',
+      next: 8,
+      prev: 6
     },
     {
+      id: 8,
       title: 'Aliança',
-      subject: 'New event: Trip to Vegas',
-      id: 3
+      icon: 'Alianca',
+      img: '2 - Aliança',
+      next: 9,
+      prev: 7
+    },
+    {
+      title: 'Provisão',
+      id: 1,
+      icon: '',
+      img: '3 - Provisão no Egito'
     },
   ];
 
